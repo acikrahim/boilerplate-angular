126283b05f55f6a9be957d68687a62c6ca535434
Abdul Rahim Mohamad <abdulrahim@Abduls-MacBook-Pro-2.local>
2022-11-03 15:42:13 +0800

    local storage service

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,11 +1,36 @@
-// import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+// import * as CryptoJS from 'crypto-js';
 
-// @Injectable({
-//     providedIn: 'root'
-// })
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalService {
 
-// export class LocalStorageService {
-//     has(key: string): boolean {
-//         return this.get(key) ? true : false;
-//     }
-// }
+  key = "123";
+
+  constructor() { }
+
+  set(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  getData(key: string) {
+    return localStorage.getItem(key) || "";
+  }
+
+  remove(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  clear() {
+    localStorage.clear();
+  }
+
+//   private encrypt(txt: string): string {
+//     return CryptoJS.AES.encrypt(txt, this.key).toString();
+//   }
+
+//   private decrypt(txtToDecrypt: string) {
+//     return CryptoJS.AES.decrypt(txtToDecrypt, this.key).toString(CryptoJS.enc.Utf8);
+//   }
+}
